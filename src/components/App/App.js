71589edaa1c582607c01eef3f7a16dbf0bcdd3f5
Piotr Papiernik <piotrpapiernik132@gmail.com>
2021-02-20 16:37:47 +0100

    init

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+import UsersList from '../UsersList/UsersList';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <UsersList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UsersList/UsersList.js
@@ -0,0 +1,14 @@
+function UsersList() {
+  return (
+    <div>
+      <input
+        type="search"
+        name="users"
+        id="users_list"
+        placeholder="Search by user name..."
+      />
+    </div>
+  );
+}
+
+export default UsersList;
