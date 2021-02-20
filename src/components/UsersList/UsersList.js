88e5d35a4458321a68a948e0436f456e4bf6ee15
Piotr Papiernik <piotrpapiernik132@gmail.com>
2021-02-20 17:55:03 +0100

    add memoize users list; integrate with API

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,26 +1,60 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import SingleUser from '../SingleUser/SingleUser';
-import USERS from '../../USERS.json';
 import styles from './UsersList.module.css';
+import { API_URL_USERS } from '../../constants';
+
 function UsersList() {
   const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const getFilteredUsers = (searchInput) => {
+    return users.filter(
+      ({ username, name }) =>
+        username.toLowerCase().includes(searchInput.toLowerCase().trim()) ||
+        name.toLowerCase().includes(searchInput.toLowerCase().trim())
+    );
+  };
+  const memoizedUsers = useMemo(() => getFilteredUsers(searchTerm), [
+    searchTerm,
+    users,
+  ]);
 
   useEffect(() => {
-    setUsers(USERS);
+    setLoading(true);
+    fetch(API_URL_USERS)
+      .then((response) => response.json())
+      .then((res) => {
+        setLoading(false);
+        setUsers(res);
+      });
   }, []);
 
   return (
     <div className={styles.container}>
+      <h1>Users List</h1>
       <input
+        className={styles.userInput}
         type="search"
         name="users"
         id="users_list"
         placeholder="Search by user name..."
+        value={searchTerm}
+        onChange={handleChange}
       />
-      <ul>
-        {users?.map(({ id, name, username }) => (
-          <SingleUser key={id} name={name} username={username} />
-        ))}
+      <ul className={styles.listContainer}>
+        {loading ? <div>Loading ...</div> : null}
+        {!loading && memoizedUsers.length === 0 ? (
+          <div>No users found</div>
+        ) : (
+          memoizedUsers.map(({ id, name, username }) => (
+            <SingleUser key={id} name={name} username={username} />
+          ))
+        )}
       </ul>
     </div>
   );
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+const API_URL = 'https://jsonplaceholder.typicode.com';
+export const API_URL_USERS = API_URL + '/users';
